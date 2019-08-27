3fc2cac936e52985d478ddc470b5874b27ce2335
root <root@5m1.mancos.games>
2019-08-27 16:15:46 +0000

    Skincreator locale

---
+++ [esx]/[basics]/skincreator/locales/es.lua
@@ -0,0 +1,6 @@
+Locales['es'] = {
+  ['skin_menu'] = 'Menu de apariencia',
+  ['use_rotate_view'] = 'Usa ~INPUT_VEH_FLY_ROLL_LEFT_ONLY~ y ~INPUT_VEH_FLY_ROLL_RIGHT_ONLY~ para rotar la camara.',
+  ['skin'] = 'Cambio apaiencia',
+  ['saveskin'] = 'Guardar apariencia',
+}
